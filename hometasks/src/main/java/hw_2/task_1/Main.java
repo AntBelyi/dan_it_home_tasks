f069b66b842a8b00a48df78f099ca374b1a2dcb9
Anton_Bilyi <Anton_Bilyi@epam.com>
2025-07-03 19:37:04 +0300

    some java code

---
+++ hometasks/src/main/java/hw_2/task_1/Main.java
@@ -0,0 +1,7 @@
+package hw_2.task_1;
+
+public class Main {
+    public static void main(String[] args) {
+        //First task of second lecture
+    }
+}
---
+++ hometasks/src/main/java/hw_2/task_2/Program.java
@@ -0,0 +1,9 @@
+package hw_2.task_2;
+
+public class Program {
+
+    public static void main(String[] args) {
+        //Bla
+    }
+
+}
