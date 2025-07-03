1ee5e52600fe528290f2594b8666cb889767bcb7
Anton_Bilyi <Anton_Bilyi@epam.com>
2025-07-03 19:47:00 +0300

    some changes

--- hometasks/src/main/java/hw_2/task_1/Main.java
+++ hometasks/src/main/java/hw_2/task_1/Main.java
@@ -2,6 +2,6 @@ package hw_2.task_1;
 
 public class Main {
     public static void main(String[] args) {
-        //First task of second lecture
+        System.out.println("blalkam");
     }
 }
--- hometasks/src/main/java/hw_2/task_2/Program.java
+++ hometasks/src/main/java/hw_2/task_2/Program.java
@@ -3,7 +3,7 @@ package hw_2.task_2;
 public class Program {
 
     public static void main(String[] args) {
-        //Bla
+        System.out.println("fuuu");
     }
 
 }
